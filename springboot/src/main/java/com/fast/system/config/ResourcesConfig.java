a75b62a527e853731115fdc33c449dddf479cc73
H101LP <h86304585@163.com>
2026-08-06 10:44:06 +0800

    项目配置文件完善

--- springboot/src/main/java/com/fast/system/config/ResourcesConfig.java
+++ springboot/src/main/java/com/fast/system/config/ResourcesConfig.java
@@ -1,9 +1,15 @@
 package com.fast.system.config;
 
 import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
 
 /**
  * 资源处理配置类
@@ -13,7 +19,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration//告诉spring我是配置类
 public class ResourcesConfig implements WebMvcConfigurer {
-    @Resource
     private final FastConfig fastConfig;
     public ResourcesConfig(FastConfig fastConfig) {
         this.fastConfig = fastConfig;
@@ -27,5 +32,31 @@ public class ResourcesConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/profile/**")
                 .addResourceLocations("file:"+ fastConfig.getProfile()+"/");
+    }
+    /**
+     * 跨域配置源
+     * 配置哪些网站可以访问后端的API  起到通行证的作用
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        //1 创建跨域配置对象
+        CorsConfiguration config = new CorsConfiguration();
+        //2 允许所有来源
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        //3 允许携带所有请求头
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        //4 允许所有方法（GET POST PUT DELETE ...）
+        config.setAllowedMethods(Collections.singletonList("*"));
+        //5 是否允许发送凭证
+        config.setAllowCredentials(true);
+        //预检请求的缓存时间
+        config.setMaxAge(1800L);
+
+        //创建基于URL的跨域配置源
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //对所有路径应用跨域规则
+        source.registerCorsConfiguration("/**", config);
+        return source;
+
     }
 }
