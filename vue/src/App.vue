50cc1887f08e339677193c14f6c8b786e6c1ae3d
H101LP <h86304585@163.com>
2026-08-07 16:03:47 +0800

    VUE

---
+++ vue/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>这是前端根组件</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
